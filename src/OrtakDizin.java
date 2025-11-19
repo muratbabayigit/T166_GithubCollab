4f7556bab52cf36f3aa9ff32a1f6d9758667c319
Yusuf Celal <yusufcelal34@gmail.com>
2025-11-19 22:08:05 +0300

    yusuf

--- src/OrtakDizin.java
+++ src/OrtakDizin.java
@@ -2,5 +2,6 @@ public class OrtakDizin {
     public static void main(String[] args) {
         System.out.println("ne yaparsan yap AŞK ile yap");
         System.out.println("deneme");
+        System.out.println("ortalık karışmış zaten hocam");
     }
 }
